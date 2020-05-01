bf740cee2799171dea88a42fb54e80db394bc00e
dathomas1 <darnell.thomas@gmail.com>
2020-04-30 20:27:45 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+
+navToggle.addEventListener('click', () => {
+  document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    document.body.classList.remove('nav-open');
+  })
+})
+
+AOS.init({
+   duration: 800,
+   delay: 300,
+   once: true,
+   disable: 'mobile'
+}); 
